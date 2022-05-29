1d2abe051a693ff924728574f5af8f86330b75f5
Alvaro Rodrigues <alvim.lopez@gmail.com>
2022-05-29 00:23:47 -0300

    Criando o controller /salvar, testando no banco

--- src/main/java/aeetech/criptografarsenhausuario/controller/UsuarioController.java
+++ src/main/java/aeetech/criptografarsenhausuario/controller/UsuarioController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,6 +27,11 @@ public class UsuarioController {
 		
 		return ResponseEntity.ok(repository.findAll());
 		
+	}
+	
+	@PostMapping("/salvar")
+	public ResponseEntity<UsuarioModel> salvar(@RequestBody UsuarioModel usuario){
+		return ResponseEntity.ok(repository.save(usuario));
 	}
 
 }
