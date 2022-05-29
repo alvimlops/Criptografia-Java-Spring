6168626e85e2866fb5f000174d15db2f61b913ea
Alvaro Rodrigues <alvim.lopez@gmail.com>
2022-05-29 00:29:24 -0300

    Adicionando o Spring Security

--- src/main/java/aeetech/criptografarsenhausuario/CriptografarSenhaUsuarioApplication.java
+++ src/main/java/aeetech/criptografarsenhausuario/CriptografarSenhaUsuarioApplication.java
@@ -2,8 +2,9 @@ package aeetech.criptografarsenhausuario;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class CriptografarSenhaUsuarioApplication {
 
 	public static void main(String[] args) {
