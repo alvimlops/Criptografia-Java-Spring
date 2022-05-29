8ecb18edd8b1eaa664f743f2db68c88267c07c33
Alvaro Rodrigues <alvim.lopez@gmail.com>
2022-05-29 01:07:59 -0300

    Validando a senha

--- src/main/java/aeetech/criptografarsenhausuario/controller/UsuarioController.java
+++ src/main/java/aeetech/criptografarsenhausuario/controller/UsuarioController.java
@@ -1,13 +1,16 @@
 package aeetech.criptografarsenhausuario.controller;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import aeetech.criptografarsenhausuario.model.UsuarioModel;
@@ -37,5 +40,22 @@ public class UsuarioController {
 		usuario.setPassword(encoder.encode(usuario.getPassword()));
 		return ResponseEntity.ok(repository.save(usuario));
 	}
+	
+	 @GetMapping("/validarSenha")
+	    public ResponseEntity<Boolean> validarSenha(@RequestParam String login,
+	                                                @RequestParam String password) {
+
+	        Optional<UsuarioModel> optUsuario = repository.findByLogin(login);
+	        if (optUsuario.isEmpty()) {
+	            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(false);
+	        }
+
+	        UsuarioModel usuario = optUsuario.get();
+	        boolean valid = encoder.matches(password, usuario.getPassword());
+
+	        HttpStatus status = (valid) ? HttpStatus.OK : HttpStatus.UNAUTHORIZED;
+	        return ResponseEntity.status(status).body(valid);
+
+	    }
 
 }
